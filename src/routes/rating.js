c0aeecef0d8541583413a57527312de205741f6e
trung123jjj <23521677@gm.uit.edu.vn>
2026-05-02 23:53:21 +0700

    Fix rating.js: replace auth with verifyJWT

--- src/routes/rating.js
+++ src/routes/rating.js
@@ -1,17 +1,17 @@
 const ratingController = require("../controllers/ratingController");
 const express = require('express');
 const { validateRating } = require('../middleware/validate');
-const { authenticateToken } = require('../middleware/auth');
+const verifyJWT = require('../middleware/verifyJWT');
 const router = express.Router();
 
 router
     .get('/movie/:movieId', ratingController.getMovieRating)
-    .post('/', authenticateToken, ratingController.rateMovie)
-    .get('/user/:movieId', authenticateToken, ratingController.getUserRating)
-    .post('/add', authenticateToken, validateRating, ratingController.createRating)
-    .get('/get', authenticateToken, ratingController.getAllRatings)
-    .delete('/delete', authenticateToken, ratingController.deleteRating)
-    .put('/update', authenticateToken, ratingController.updateRating)
+    .post('/', verifyJWT, ratingController.rateMovie)
+    .get('/user/:movieId', verifyJWT, ratingController.getUserRating)
+    .post('/add', verifyJWT, validateRating, ratingController.createRating)
+    .get('/get', verifyJWT, ratingController.getAllRatings)
+    .delete('/delete', verifyJWT, ratingController.deleteRating)
+    .put('/update', verifyJWT, ratingController.updateRating)
     .post('/batch', ratingController.getBatchMovieRatings);
 
 module.exports = router;

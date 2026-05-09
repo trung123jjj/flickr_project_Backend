234a6a3446dd521822bf1656854aa0e68b4f2ff3
trung123jjj <23521677@gm.uit.edu.vn>
2026-05-09 14:30:46 +0700

    Update errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,8 +1,8 @@
-const { logEvents } = require("./logEvents");
+const { logEvents } = require('./logEvents')
 
 const errorHandler = (error, req, res, next) => {
-  logEvents(`${error.name}: ${error.message}`, "errorLog.txt");
-  res.status(500).json({ message: error.message || "Internal Server Error" });
-};
+  logEvents(`${error.name}: ${error.message}`, 'errorLog.txt')
+  res.status(500).send(error.message)
+}
 
-module.exports = errorHandler;
+module.exports = errorHandler
